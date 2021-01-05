1169c87df8252ccde27fe42b6083e03a99241e73
ole <lekakeny.karoi@gmail.com>
2021-01-05 09:02:25 +0300

    first commit

--- mainsite/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/olekaroi/PycharmProjects/mock_endpoint_interview/mainsite/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
